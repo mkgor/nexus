1213cfb5c08ac7638f6860bded83621126ad2a66
Gor Mkhitaryan <gor.mkhitaryan@rt.ru>
2022-04-18 14:00:58 +0300

    Added ignore comment for analyzer

--- nexus_codegen/lib/src/templates/state.dart
+++ nexus_codegen/lib/src/templates/state.dart
@@ -17,6 +17,8 @@ class StateTemplate {
   @override
   String toString() {
     return """
+    // ignore_for_file: constant_identifier_names, unnecessary_this,public_member_api_docs, non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
 class ${name}Properties {
   ${reactiveTemplates?.map((e) => "static const ${e.name.startsWith("_") ? "\$${e.name}" : e.name} = '${e.name}';").join("\n")}
 }
